923e085ad05f6de823d1150642cb165390fe6997
safimiazi <mohibullamiazi@gmail.com>
2025-08-30 16:17:12 +0600

    Auto commit: Modified src/pages/dashboard/home/get_clips/GetClips.tsx

--- src/pages/dashboard/home/get_clips/GetClips.tsx
+++ src/pages/dashboard/home/get_clips/GetClips.tsx
@@ -46,7 +46,7 @@ const GetClips = () => {
           )}
 
           {/* Action Buttons */}
-          {activeTab === "clips" && (
+          {/* {activeTab === "clips" && (
             <div className="flex flex-wrap gap-2 w-full md:w-auto">
               <button className="flex items-center gap-2 bg-black text-white px-4 py-2 rounded-lg transition duration-200 w-full md:w-auto justify-center">
                 <img src={select} alt="" className="w-5 h-5" />
@@ -66,7 +66,7 @@ const GetClips = () => {
                 <img src={threedot} alt="" className="w-5 h-5 mx-auto" />
               </button>
             </div>
-          )}
+          )} */}
         </div>
 
         {/* Tabs */}
