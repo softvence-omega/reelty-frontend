ba263b220b155b3ce63194ba24a9f82c74951b87
Arman8957 <novicethought0@gmail.com>
2025-10-16 17:10:57 +0600

    updated the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,9 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 8080,
+    port: 3000,
     strictPort: true,
-    origin: "http://0.0.0.0:8080",
+    origin: "http://0.0.0.0:3000",
     allowedHosts: [
       "api.reelty.com.au",
       "reelty.com.au",
